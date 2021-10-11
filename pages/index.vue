4cb96aaffc917b680f2dc3662b66473008028adb
Lars Rickert <lars.rickert@web.de>
2021-10-11 10:57:30 +0200

    renamed AppCarousel to Carousel

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
-  <AppCarousel :slides="slides" />
+  <Carousel :slides="slides" />
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from '@nuxtjs/composition-api'
-import AppCarousel from '~/components/AppCarousel.vue'
+import Carousel from '~/components/Carousel.vue'
 import { CarouselSlide } from '~/types/carousel'
 
 export default defineComponent({
-  components: { AppCarousel },
+  components: { Carousel },
   setup() {
     const slides = ref<CarouselSlide[]>([
       {
