c8d1b54fea839a1500d5134d7448e5ac0b640f2f
Lars Rickert <lars.rickert@web.de>
2021-10-11 11:01:49 +0200

    added search to navbar, made it dark

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar toggleable="lg" type="dark" variant="dark">
       <b-navbar-brand>NavBar</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
@@ -15,10 +15,20 @@
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-item-dropdown text="Sprache" right>
+          <b-nav-form>
+            <b-form-input
+              size="me"
+              class="mr-sm-2"
+              placeholder="Search"
+              type="search"
+            ></b-form-input>
+            <b-button size="me" type="submit">Search</b-button>
+          </b-nav-form>
+
+          <!-- <b-nav-item-dropdown text="Sprache" right>
             <b-dropdown-item active href="#">DE</b-dropdown-item>
             <b-dropdown-item href="#">EN</b-dropdown-item>
-          </b-nav-item-dropdown>
+          </b-nav-item-dropdown> -->
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
