b5de70869c1178adea41175f25f8e38fdc418ebe
Lars Rickert <lars.rickert@web.de>
2021-10-11 12:20:48 +0200

    added footer

---
+++ components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="container">
+    <p class="float-right"><a href="#">Back to top</a></p>
+    <p>
+      &copy; 2017–2021 Company, Inc. &middot; <a href="#">Privacy</a> &middot;
+      <a href="#">Terms</a>
+    </p>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {},
+})
+</script>
+
+<style scoped>
+footer {
+  padding-bottom: 3rem;
+}
+</style>
