60ed5b39dcf57b4446cc9ad4121c1931bdf6f59d
Lars Rickert <lars.rickert@web.de>
2021-10-20 10:52:19 +0200

    added hero

---
+++ pages/landing-page.vue
@@ -0,0 +1,30 @@
+<template>
+  <main>
+    <Hero
+      :src="require('~/assets/hero.jpg')"
+      title="Our product or service"
+      subtitle="Lorem ipsum dolor sit amet"
+      :btn="heroBtn"
+    />
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import Hero from '~/components/Hero.vue'
+import { IButton } from '~/types/misc'
+
+export default defineComponent({
+  components: { Hero },
+  setup() {
+    const heroBtn: IButton = {
+      to: '/',
+      text: 'Click me',
+    }
+
+    return { heroBtn }
+  },
+})
+</script>
+
+<style scoped></style>
