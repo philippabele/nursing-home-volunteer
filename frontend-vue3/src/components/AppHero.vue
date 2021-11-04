6b5b294a731290a4ad712024eca5341ff82ba084
Lars Rickert <67898185+larsrickert@users.noreply.github.com>
2021-11-04 17:38:22 +0100

    added hero

---
+++ frontend-vue3/src/components/AppHero.vue
@@ -0,0 +1,62 @@
+<script lang="ts" setup>
+import { IButton } from '../types/components'
+
+interface HeroProps {
+  src: string
+  title: string
+  subtitle?: string
+  btn?: IButton
+  height?: number
+}
+
+withDefaults(defineProps<HeroProps>(), {
+  height: 650,
+  subtitle: '',
+  btn: undefined,
+})
+</script>
+
+<template>
+  <section :style="{ backgroundImage: `url(${src})`, height: `${height}px` }" class="hero">
+    <div class="hero__overlay d-flex justify-content-center align-items-center text-center">
+      <div class="hero__content">
+        <h1>{{ title }}</h1>
+        <p v-if="subtitle" class="mt-4">{{ subtitle }}</p>
+
+        <b-button
+          v-if="btn"
+          class="mt-4 px-5"
+          :to="!btn.external ? btn.to : undefined"
+          :href="btn.external ? btn.to : undefined"
+          variant="primary"
+        >
+          {{ btn.text }}
+        </b-button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.hero {
+  width: 100%;
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  color: #ffffff;
+
+  &__overlay {
+    width: 100%;
+    height: 100%;
+
+    /* overlay */
+    background: rgba(0, 0, 0, 0.4) 100%;
+  }
+
+  &__content {
+    p {
+      font-size: 18px;
+    }
+  }
+}
+</style>
--- frontend-vue3/src/views/HomePage.vue
+++ frontend-vue3/src/views/HomePage.vue
@@ -1,7 +1,23 @@
-<script setup lang="ts"></script>
+<script lang="ts" setup>
+import heroImgUrl from '../assets/hero.jpg'
+import AppHero from '../components/AppHero.vue'
+import { IButton } from '../types/components'
+
+const heroBtn: IButton = {
+  to: '/',
+  text: 'Click me',
+}
+</script>
 
 <template>
-  <div>Home page</div>
+  <main>
+    <AppHero
+      :src="heroImgUrl"
+      title="Our product or service"
+      subtitle="Lorem ipsum dolor sit amet"
+      :btn="heroBtn"
+    />
+  </main>
 </template>
 
-<style lang="scss"></style>
+<style scoped lang="scss"></style>
