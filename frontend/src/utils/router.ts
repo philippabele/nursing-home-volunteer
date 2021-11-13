4d666dd94e88e2765cdc0e0496f37025a67af4b7
Lars Rickert <67898185+larsrickert@users.noreply.github.com>
2021-11-13 12:35:09 +0100

    added function return type

--- frontend/src/utils/router.ts
+++ frontend/src/utils/router.ts
@@ -1,6 +1,9 @@
 import router from '../router'
 
-export async function navigate(href: string, target = '_blank' as '_self' | '_blank') {
+export async function navigate(
+  href: string,
+  target = '_blank' as '_self' | '_blank'
+): Promise<void> {
   if (href.startsWith('http')) window.open(href, target)
   else {
     await router.push(href)
