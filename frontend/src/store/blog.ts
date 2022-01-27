c9226f16271f599c94a38912297a5013b27e7eb8
Lars Rickert <67898185+larsrickert@users.noreply.github.com>
2022-01-27 20:55:22 +0100

    removed console log

--- frontend/src/store/blog.ts
+++ frontend/src/store/blog.ts
@@ -17,9 +17,8 @@ export const useBlogStore = defineStore('blog', {
 
       try {
         const { data } = await axiosInstance.get<IBlogPost[]>('blog-posts')
-        console.log(data)
 
-        // strapi medai (images, videos etc.) urls are only relative paths, so add the api host here
+        // strapi media (images, videos etc.) urls are only relative paths, so add the api host here
         data.forEach((post) => {
           if (post.featuredImage) {
             post.featuredImage = addApiHostToMedia(post.featuredImage)
