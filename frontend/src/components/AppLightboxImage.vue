c6bc1cb124abb3ce83b1e1aa2b7af00c2807625c
Lars Rickert <67898185+larsrickert@users.noreply.github.com>
2022-02-08 15:09:52 +0100

    added lightbox component

---
+++ frontend/src/components/AppLightboxImage.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import VueEasyLightbox from 'vue-easy-lightbox'
+
+interface MediaSectionProps {
+  src: string
+  alt: string
+}
+
+defineProps<MediaSectionProps>()
+
+const isLightBoxVisible = ref(false)
+</script>
+
+<template>
+  <div>
+    <vue-easy-lightbox
+      scroll-disabled
+      move-disabled
+      :visible="isLightBoxVisible"
+      :imgs="src"
+      @hide="isLightBoxVisible = false"
+    ></vue-easy-lightbox>
+    <img :src="src" class="img-fluid rounded" :alt="alt" @click="isLightBoxVisible = true" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+img {
+  cursor: pointer;
+}
+</style>
